2c3e84296bfc5bf3d3735b960f2863c5c0b83fac
Spencer Curtis <spencercolecurtis@gmail.com>
2017-05-17 23:28:38 -0600

    Initial Commit

---
+++ Notifile/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Notifile
+//
+//  Created by Spencer Curtis on 5/17/17.
+//  Copyright © 2017 Spencer Curtis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Notifile/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Notifile
+//
+//  Created by Spencer Curtis on 5/17/17.
+//  Copyright © 2017 Spencer Curtis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
